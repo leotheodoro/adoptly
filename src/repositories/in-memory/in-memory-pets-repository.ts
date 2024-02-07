ea20bed3814728bbef2d948309757ea50e6c1fdc
Leonardo Theodoro <leonardo@recruitrobin.com>
2024-02-06 22:15:27 -0300

    feat: if bio non existent set null

--- src/repositories/in-memory/in-memory-pets-repository.ts
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -1,5 +1,5 @@
 import { Pet, Prisma } from '@prisma/client'
-import { PetsRepository } from '../ pets-repository'
+import { PetsRepository } from '../pets-repository'
 import { randomUUID } from 'crypto'
 
 export class InMemoryPetsRepository implements PetsRepository {
@@ -11,7 +11,7 @@ export class InMemoryPetsRepository implements PetsRepository {
     const pet = {
       id: randomUUID(),
       name: data.name,
-      bio: data.bio,
+      bio: data.bio ?? null,
       type: data.type,
       age: data.age,
       energy_level: data.energy_level,
