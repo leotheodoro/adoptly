459a4ac4c32fe4a2f3381418e0452648ae0704c9
Leonardo Theodoro <leonardo@recruitrobin.com>
2024-04-25 16:12:20 -0300

    wip upload images

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -4,8 +4,13 @@ import { validateJWT } from '@/http/middlewares/validate-jwt'
 import { createAdoptionRequirementsByPet } from './create-adoption-requirements-by-pet'
 import { fetchPetsByCity } from './fetch-pets-by-city'
 import { petDetails } from './pet-details'
+import multer from 'multer'
 
 const petRoutes = Router()
+
+const upload = multer({
+  dest: './tmp',
+})
 
 petRoutes.post('/pets', validateJWT, createPet)
 petRoutes.post(
@@ -15,5 +20,16 @@ petRoutes.post(
 )
 petRoutes.get('/pets', fetchPetsByCity)
 petRoutes.get('/pets/:id', petDetails)
+petRoutes.put(
+  '/pets/upload',
+  upload.array('images', 5),
+  (request, response) => {
+    const { file } = request
+
+    console.log(file)
+
+    return response.send()
+  },
+)
 
 export default petRoutes
